1198863f5e61bf90e5746cde8ec8d0f3531be5c2
santiadlv <49079265+santiadlv@users.noreply.github.com>
2021-04-09 16:57:51 -0600

    Initial commit

---
+++ src/test/java/mx/tec/kata/StringCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.tec.kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
