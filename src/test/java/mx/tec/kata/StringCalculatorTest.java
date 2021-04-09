dc71aa9c021f22a70fa19febb1b44b543b991e3f
santiadlv <49079265+santiadlv@users.noreply.github.com>
2021-04-09 17:22:24 -0600

    Challenge 2

--- src/test/java/mx/tec/kata/StringCalculatorTest.java
+++ src/test/java/mx/tec/kata/StringCalculatorTest.java
@@ -34,4 +34,13 @@ class StringCalculatorTest {
 		
 		assertEquals(expectedResult, actualResult);
 	}
+	
+	@Test
+	void givenCalculatorWhenAdd1_3_5_7_9Then25() {
+		int expectedResult = 25;
+		String numbers = "1,3,5,7,9";
+		int actualResult = calculator.add(numbers);
+		
+		assertEquals(expectedResult, actualResult);
+	}
 }
